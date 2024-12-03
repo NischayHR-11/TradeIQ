37eea5ecab0f41a67713e19f89e2db3e996f4edd
NischayHR-11 <nischayhr11@gmail.com>
2024-12-04 01:00:18 +0530

    Implemented Hero And Award Component

--- Frontend/src/LandingPage/home/award.jsx
+++ Frontend/src/LandingPage/home/award.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 function Award() {
     return ( 
-        <>
-        <h1>Award</h1>
-        </>
+        <div className='container mt-5'>
+
+            <div className='row'>
+
+                <div className='col'>
+                
+                <img src="/media/images/console.png" alt="" />
+                </div>
+
+                <div className='col mt-5 ms-3'>
+                <h1>alkjsnv;sakf</h1>
+                <p>asjhfbasldbasfjd</p>
+                </div>
+
+            </div>
+
+        </div>
      );
 }
 
--- Frontend/src/LandingPage/home/hero.jsx
+++ Frontend/src/LandingPage/home/hero.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <>
-        <h1>Hero</h1>
-        </>
+        <div className='container mt-5'>
+        
+        <div className='row '>
+
+            <div className='col-12' style={{textAlign:"center"}}>
+
+                <img src='/media/images/homeHero.png' style={{width:"100%"}} className='mb-3'></img>
+                <h1 className='mt-5 mb-3'>Invest In Everything </h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button className='btn btn-primary mt-3 px-4 py-2'> <h6>SignUp For Free</h6></button>
+            </div>
+        </div>
+        </div>
      );
 }
 

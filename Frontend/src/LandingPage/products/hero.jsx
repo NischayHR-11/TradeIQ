f7cd5c60fc4110125aefdb18e6a26686e3e273b9
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 22:33:45 +0530

    Implemented Hero Component

--- Frontend/src/LandingPage/products/hero.jsx
+++ Frontend/src/LandingPage/products/hero.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <div className='container mt-5 ' style={{textAlign:"center"}} >
+        <div className='container mt-5 mb-5' style={{textAlign:"center"}} >
             
             <h2 className='mb-3'>TradeIQ Products</h2>
-            <p className='fs-4 text-muted'>
+            <p className='fs-md-4 text-muted'>
             Sleek, modern, and intuitive trading platforms
             </p>
             <p className='fs-6'>
             Check out our <a href="/" style={{textDecoration:"none"}}> investment offerings<i class="fa-solid fa-arrow-right ms-2 fs-6"></i></a>
             </p>
+            <hr className='mt-5 mb-5'/>
         </div>
      );
 }

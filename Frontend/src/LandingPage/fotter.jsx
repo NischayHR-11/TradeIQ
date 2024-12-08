c2c2cf3ccfc6c0a950c69615947934cf23d997d6
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 11:37:33 +0530

    Implemented Styling for Navbar  Component

--- Frontend/src/LandingPage/fotter.jsx
+++ Frontend/src/LandingPage/fotter.jsx
@@ -2,10 +2,90 @@ import React from 'react';
 
 function Fotter() {
     return ( 
-        <>
-        <h1>Fotter</h1>
-        </>
-     );
+
+            <div className="row  border-top mt-5 p-5" style={{width:"100%",backgroundColor:"#fbfbfb"}}>
+
+                <div className="col mt-5 ms-5">
+
+                <h2>TradeIQ</h2>
+
+                <p className='text-muted fs-15 pe-5 mt-3'>© 2010 - 2024, TradeIQ Broking Ltd.
+
+                All rights reserved.</p>
+                
+                <p>
+                
+                <a href="#" style={{textDecoration:"none" , color:"black"}}><i class="fa-brands fa-facebook me-3"></i></a>
+                <a href="#" style={{textDecoration:"none" , color:"black"}}><i class="fa-brands fa-instagram me-3"></i></a>
+                <a href="#" style={{textDecoration:"none" , color:"black"}}><i class="fa-brands fa-youtube me-3"></i></a>
+                <a href="#" style={{textDecoration:"none" , color:"black"}}><i class="fa-brands fa-whatsapp me-3"></i></a>
+                <a href="#" style={{textDecoration:"none" , color:"black"}}><i class="fa-solid fa-envelope me-3"></i></a>
+                </p>
+
+                </div>
+                 
+                <div className="col mt-4 mb-2">
+
+                    <h6 className='mb-4'>Company</h6>
+                    <p className='text-muted'>
+
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>About</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Products</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Pricing</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Referral programme</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Careers</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>TradeIQ.tech</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Press & media</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>TradeIQ Cares (CSR)</a><br />
+                    </p>
+                    
+                </div>
+
+                <div className="col mt-3 mb-3">
+
+                    <h6 className='mb-4'>Support</h6>
+
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Contact us</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Support portal</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Z-Connect blog</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>List of charges</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Downloads & resources</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Videos</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Market overview</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>How to file a complaint?</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Status of your complaints</a><br />
+                </div>
+
+                <div className="col mt-4">
+                    
+                    <h6 className='mb-4'>Account</h6>
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Contact us</a><br />
+                    <a href='#' style={{textDecoration:"none" , color:"black"}}>Support portal</a><br />
+
+                </div>
+                
+                <div className="row p-3">
+
+                <p className='text-muted px-5 ' style={{fontSize:"12px"}}>
+                TradeIQ Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through TradeIQ Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading through TradeIQ Commodities Pvt. Ltd. MCX: 46025; NSE-50001 – SEBI Registration no.: INZ000038238 Registered Address: TradeIQ Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@TradeIQ.com, for DP related to dp@TradeIQ.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF
+                </p>
+                <p className='text-muted px-5' style={{fontSize:"12px"}}>
+                Investments in securities market are subject to market risks; read all the related documents carefully before investing.
+                </p>
+
+                <p className='text-muted px-5' style={{fontSize:"12px"}}>
+                Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.
+                </p>
+
+                <p className='text-muted px-5' style={{fontSize:"12px"}}>
+                "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of TradeIQ and offering such services, please create a ticket here.
+                </p>
+
+                </div>
+                
+            </div>
+            
+        );
 }
 
 export default Fotter;

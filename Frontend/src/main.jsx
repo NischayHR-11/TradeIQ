9011e3db10133a70c32f0c0383d6b5b41767503c
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 22:36:12 +0530

    Path To Products Page Changed

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,9 +7,9 @@ import Fotter from './LandingPage/fotter.jsx'
 import Homepage from './LandingPage/home/homepage.jsx'
 import Signup from './LandingPage/signup/signup.jsx'
 import AboutPage from './LandingPage/about/aboutpage.jsx'
-import Hero from "./LandingPage/products/hero.jsx"
 import Hero1 from "./LandingPage/pricing/hero.jsx"
 import Hero2 from './LandingPage/support/hero.jsx'
+import Productpage from './LandingPage/products/productspage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -20,7 +20,7 @@ createRoot(document.getElementById('root')).render(
     <Route path='/' element={<Homepage></Homepage>}></Route>
     <Route path='/signup' element={<Signup></Signup>}></Route>
     <Route path='/about' element={<AboutPage></AboutPage>}></Route>
-    <Route path='/products' element={<Hero></Hero>}></Route>
+    <Route path='/products' element={<Productpage></Productpage>}></Route>
     <Route path='/pricing' element={<Hero1></Hero1>}></Route>
     <Route path='/support' element={<Hero2></Hero2>}></Route>
     <Route path='*' element={<Homepage></Homepage>}></Route>

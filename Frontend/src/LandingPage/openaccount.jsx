5eaf04f2ff37a6a4b8c000b2fa5471ba48f273a8
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 11:38:42 +0530

    Implemented Open account  Component

--- Frontend/src/LandingPage/openaccount.jsx
+++ Frontend/src/LandingPage/openaccount.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 function Openaccount() {
     return ( 
-        <>
-        <h1>Openaccount</h1>
-        </>
+        <div className='container mt-5 mb-5'>
+        
+        <div className='row '>
+
+            <div className='col-12 mb-5' style={{textAlign:"center"}}>
+
+                <h1 className='mt-5 mb-3'>Open a TradeIQ account </h1>
+                <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+                <button className='btn btn-primary mt-3 px-4 py-2'> <h6>SignUp For Free</h6></button>
+            </div>
+        </div>
+        </div>
      );
 }
 

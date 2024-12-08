93cd4a5f4517c03dab25543d014857b8efc6e425
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 18:12:25 +0530

    Made The Component Responsie For All devices

--- Frontend/src/LandingPage/home/stats.jsx
+++ Frontend/src/LandingPage/home/stats.jsx
@@ -6,7 +6,7 @@ function Stats() {
             
             <div className="row mt-5">
 
-                <div className="col-5 mt-5 me-5 p-3">
+                <div className="col-md-5 mt-5 me-5 p-3">
                 <h2>Trust with confidence</h2>
                 <h4 className='mt-5'>Customer-first always</h4>
                 <p className='text-muted'>That's why 1.5+ crore customers trust TradeIQ with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
@@ -17,13 +17,13 @@ function Stats() {
                 <h4 className='mt-5'>Do better with money</h4>
                 <p className='text-muted'>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
                 </div>
-                <div className="col-6 ms-5 mt-5">
+                <div className="col-md-6 ms-md-5 mt-5">
                 <img src="/media/images/ecosystem.png" alt=""  style={{width:"90%"}}/>
                 <div className="row  mt-3"  style={{marginLeft:"6rem"}}>
-                    <div className="col">
+                    <div className="col-md-6 mt-2">
                         <a href="#" style={{textDecoration:"none"}}>Explore Our Products <i class="fa-solid fa-arrow-right ms-2"></i></a>
                     </div>
-                    <div className="col">
+                    <div className="col-md-6 mt-2">
                         <a href="#" style={{textDecoration:"none"}}>Try Kite demo<i class="fa-solid fa-arrow-right ms-2"></i></a>
                     </div>
                 </div>

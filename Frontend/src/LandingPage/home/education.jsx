f17cffad8e26d56217111b48508b0ec2e31e7551
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 18:11:54 +0530

    Made The Component Responsie For All devices

--- Frontend/src/LandingPage/home/education.jsx
+++ Frontend/src/LandingPage/home/education.jsx
@@ -6,11 +6,11 @@ function Education() {
 
             <div className="row">
 
-                <div className="col">
-                <img src="\media\images\education.svg" alt="" />
+                <div className="col-md-6">
+                <img src="\media\images\education.svg" alt=""  style={{width:"90%"}}/>
                 </div>
 
-                <div className="col mt-5">
+                <div className="col-md-6 mt-5">
                 <h2>Free and open market education</h2>
                 <p className='mt-4'>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
                 <a href="#" style={{textDecoration:"none"}}> Varsity <i class="fa-solid fa-arrow-right ms-2"></i></a>

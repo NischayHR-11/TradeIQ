0ca2a5a372464f451ff5e5f5869ed0b557ec09db
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 11:33:02 +0530

    Implemented Education Component

--- Frontend/src/LandingPage/home/education.jsx
+++ Frontend/src/LandingPage/home/education.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 function Education() {
     return ( 
-        <>
-        <h1>Education</h1>
-        </>
+        <div className='container mt-5 mb-5'>
+
+            <div className="row">
+
+                <div className="col">
+                <img src="\media\images\education.svg" alt="" />
+                </div>
+
+                <div className="col mt-5">
+                <h2>Free and open market education</h2>
+                <p className='mt-4'>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
+                <a href="#" style={{textDecoration:"none"}}> Varsity <i class="fa-solid fa-arrow-right ms-2"></i></a>
+                <p className='mt-4'>TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
+                <a href="#" style={{textDecoration:"none"}}> TradingQ&A  <i class="fa-solid fa-arrow-right ms-2"></i></a>
+                </div>
+
+            </div>
+        
+        </div>
      );
 }
 

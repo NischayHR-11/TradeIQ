8aceb40999ac618157a9fdbe5b04fb4d00f94798
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 11:35:16 +0530

    Implemented HomePage  Component

--- Frontend/src/LandingPage/home/homepage.jsx
+++ Frontend/src/LandingPage/home/homepage.jsx
@@ -5,20 +5,16 @@ import Stats from './stats';
 import Pricing from './pricing';
 import Education from './education';
 import Openaccount from '../openaccount';
-import Navbar from '../navbar';
-import Fotter from '../fotter';
 
 function Homepage() {
     return ( 
         <>
-        <Navbar></Navbar>
         <Hero></Hero>
         <Award></Award>
         <Stats></Stats>
         <Pricing></Pricing>
         <Education></Education>
         <Openaccount></Openaccount>
-        <Fotter></Fotter>
         </>
      );
 }

ffaa8e600056cff1a464322bf7be6770c0b21bb8
NischayHR-11 <nischayhr11@gmail.com>
2024-12-08 11:39:48 +0530

    Added React Routes For The Pages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 import './index.css'
-import App from './App.jsx'
+import Navbar from './LandingPage/navbar.jsx'
+import Fotter from './LandingPage/fotter.jsx'
+import Homepage from './LandingPage/home/homepage.jsx'
+import Signup from './LandingPage/signup/signup.jsx'
+import AboutPage from './LandingPage/about/aboutpage.jsx'
+import Hero from "./LandingPage/products/hero.jsx"
+import Hero1 from "./LandingPage/pricing/hero.jsx"
+import Hero2 from './LandingPage/support/hero.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+
+  <Navbar></Navbar>
+  <Routes>
+
+    <Route path='/' element={<Homepage></Homepage>}></Route>
+    <Route path='/signup' element={<Signup></Signup>}></Route>
+    <Route path='/about' element={<AboutPage></AboutPage>}></Route>
+    <Route path='/products' element={<Hero></Hero>}></Route>
+    <Route path='/pricing' element={<Hero1></Hero1>}></Route>
+    <Route path='/support' element={<Hero2></Hero2>}></Route>
+    <Route path='*' element={<Homepage></Homepage>}></Route>
+  </Routes>
+  <Fotter></Fotter>
+  </BrowserRouter>
 )

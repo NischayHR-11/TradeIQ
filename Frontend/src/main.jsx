7eef4b8b977e6bf0bf16b267fa2727e60bbd968d
NischayHR-11 <nischayhr11@gmail.com>
2024-12-10 01:25:32 +0530

    Changed The Path Of The Route

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import './index.css'
@@ -7,9 +6,9 @@ import Fotter from './LandingPage/fotter.jsx'
 import Homepage from './LandingPage/home/homepage.jsx'
 import Signup from './LandingPage/signup/signup.jsx'
 import AboutPage from './LandingPage/about/aboutpage.jsx'
-import Hero1 from "./LandingPage/pricing/hero.jsx"
 import Hero2 from './LandingPage/support/hero.jsx'
 import Productpage from './LandingPage/products/productspage.jsx'
+import Pricingpage from './LandingPage/pricing/pricingpage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -21,7 +20,7 @@ createRoot(document.getElementById('root')).render(
     <Route path='/signup' element={<Signup></Signup>}></Route>
     <Route path='/about' element={<AboutPage></AboutPage>}></Route>
     <Route path='/products' element={<Productpage></Productpage>}></Route>
-    <Route path='/pricing' element={<Hero1></Hero1>}></Route>
+    <Route path='/pricing' element={<Pricingpage></Pricingpage>}></Route>
     <Route path='/support' element={<Hero2></Hero2>}></Route>
     <Route path='*' element={<Homepage></Homepage>}></Route>
   </Routes>

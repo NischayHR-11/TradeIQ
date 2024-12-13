9be639e8aac43b6a77a7c4c73233d2fb5c0cfb4a
NischayHR-11 <nischayhr11@gmail.com>
2024-12-13 12:28:11 +0530

    Added Ticket Component OF Support Page

--- Frontend/src/LandingPage/support/ticket.jsx
+++ Frontend/src/LandingPage/support/ticket.jsx
@@ -1,10 +1,89 @@
 import React from 'react';
+import "../../index.css"
 
 function Ticket() {
     return ( 
-        <>
-        <h1>Ticket</h1>
-        </>
+        <div className='conatiner ' style={{width:"90%"}}>
+            
+            <p className='mt-5 mb-5 ms-md-5 text-muted fs-4 ps-5'>TO Create a ticket , Select a Relevant topic .</p>
+
+            <div className="row mb-5 anc" id='anc'  style={{width:"100%"}}>
+
+                <div className="col-md-1 mt-md-3">
+
+                </div>
+
+                <div className="col-md-4 ps-5 mt-5">
+                <h5 className='mb-4 text-muted'> <i class="fa-solid fa-circle-plus me-3"></i>Account Opening </h5>
+                <a href=""  style={{textDecoration:"none",marginTop:"3rem"}} >Getting started</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Online</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Offline</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Charges</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Company, Partnership and HUF</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Non Resident Indian (NRI)</a><br />
+                </div>
+
+                <div className="col-md-4 mt-5 ps-5">
+                <h5 className='mb-4 text-muted'><i class="fa-regular fa-user me-3"></i> Your Zerodha Account</h5>
+                <a href=""  style={{textDecoration:"none",marginTop:"3rem"}} >Getting started</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Online</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Offline</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Charges</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Company, Partnership and HUF</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Non Resident Indian (NRI)</a><br />
+                </div>
+
+                <div className="col-md-3 mt-5 ps-5">
+                <h5 className='mb-4 text-muted'><i class="fa-solid fa-arrow-trend-up me-3"></i>  Trading and Markets </h5>
+                <a href=""  style={{textDecoration:"none",marginTop:"3rem"}} >Getting started</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Online</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Offline</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Charges</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Company, Partnership and HUF</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Non Resident Indian (NRI)</a><br />
+                </div>
+
+            </div>
+
+            <div className="row  mt-5" style={{width:"100"}}>
+
+                <div className="col-md-1">
+
+                </div>
+
+                <div className="col-md-4 ps-5 mt-5">
+                <h5 className='mb-4 text-muted'><i class="fa-solid fa-wallet me-3"></i>Funds</h5>
+                <a href=""  style={{textDecoration:"none",marginTop:"3rem"}} >Getting started</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Online</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Offline</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Charges</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Company, Partnership and HUF</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Non Resident Indian (NRI)</a><br />
+                </div>
+
+                <div className="col-md-4 ps-5 mt-5">
+                <h5 className='mb-4 text-muted'><i class="fa-solid fa-terminal me-3"></i>  Console</h5>
+                <a href=""  style={{textDecoration:"none",marginTop:"3rem"}} >Getting started</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Online</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Offline</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Charges</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Company, Partnership and HUF</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Non Resident Indian (NRI)</a><br />
+                </div>
+
+                <div className="col-md-3 ps-5 mt-5 ">
+                <h5 className='mb-4 text-muted'><i class="fa-solid fa-coins me-3"></i>  Coin </h5>
+                <a href=""  style={{textDecoration:"none",marginTop:"3rem"}} >Getting started</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Online</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Offline</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Charges</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Company, Partnership and HUF</a><br />
+                <a href=""  style={{textDecoration:"none",marginTop:"1rem"}} >Non Resident Indian (NRI)</a><br />
+                </div>
+
+            </div>
+            
+        </div>
      );
 }
 

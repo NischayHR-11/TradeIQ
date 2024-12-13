b4fcdf665551caa20c1b0c59ab3b974b70dbe387
NischayHR-11 <nischayhr11@gmail.com>
2024-12-13 12:30:16 +0530

    Changed The Route Of Home Page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,9 +6,9 @@ import Fotter from './LandingPage/fotter.jsx'
 import Homepage from './LandingPage/home/homepage.jsx'
 import Signup from './LandingPage/signup/signup.jsx'
 import AboutPage from './LandingPage/about/aboutpage.jsx'
-import Hero2 from './LandingPage/support/hero.jsx'
 import Productpage from './LandingPage/products/productspage.jsx'
 import Pricingpage from './LandingPage/pricing/pricingpage.jsx'
+import SupportPage from './LandingPage/support/supportpage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -21,7 +21,7 @@ createRoot(document.getElementById('root')).render(
     <Route path='/about' element={<AboutPage></AboutPage>}></Route>
     <Route path='/products' element={<Productpage></Productpage>}></Route>
     <Route path='/pricing' element={<Pricingpage></Pricingpage>}></Route>
-    <Route path='/support' element={<Hero2></Hero2>}></Route>
+    <Route path='/support' element={<SupportPage></SupportPage>}></Route>
     <Route path='*' element={<Homepage></Homepage>}></Route>
   </Routes>
   <Fotter></Fotter>
